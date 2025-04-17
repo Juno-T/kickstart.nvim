655b9e6347850af54b0397522bcb44fd34d65489
juno-t <yossathorn.tian@gmail.com>
2025-04-17 19:25:27 +0900

    add lazygit current file project

--- init.lua
+++ init.lua
@@ -1053,6 +1053,7 @@ require('lazy').setup({
     -- order to load the plugin when the command is run for the first time
     keys = {
       { '<leader>tg', '<cmd>LazyGit<cr>', desc = '[T]oggle Lazy[G]it' },
+      { '<leader>tG', '<cmd>LazyGitCurrentFile<cr>', desc = '[T]oggle Lazy[G]it on cur. proj.' },
     },
   },
 }, {
