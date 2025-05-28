5a5e2438155736cab2b3d6de30bc887819eebcf9
juno-t <yossathorn.tian@gmail.com>
2025-05-28 22:54:17 +0900

    Use catppuccin-mocha

--- init.lua
+++ init.lua
@@ -1177,7 +1177,7 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'tokyonight-night'
-      vim.cmd.colorscheme 'catppuccin-macchiato'
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 
